ad75b9aca6e53957025d8919ebc1c93e95032a49
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2023-01-18 15:13:39 -0500

    celcius to farenheight

---
+++ Celcius To Farrenheight/index.js
@@ -0,0 +1,9 @@
+var button = document.querySelector('button');
+const celc = document.getElementById('tbuser');
+const out1 = document.getElementById('output1');
+let faren = 0;
+
+function fub1() {
+    faren = celc.value * (9/5) + 32;
+    out1.innerHTML = faren;
+}
