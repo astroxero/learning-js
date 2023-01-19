358eb9c224ce4a9d65fb2fe1a8ce8cba238beded
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2023-01-18 20:11:01 -0500

    styling

--- Note Taker/index.js
+++ Note Taker/index.js
@@ -4,13 +4,13 @@ let i = document.getElementById('noteInput')
 function commitTheNote() {
     let newDiv = document.createElement("div")
     let newButton = document.createElement("button")
-    newButton.placeholder = 'X'
+    newButton.innerText = 'X'
     newDiv.innerHTML = i.value;
     document.body.appendChild(newDiv)
     document.body.appendChild(newButton)
     newButton.onclick = function deleteNewDiv() {
-    document.body.removeChild(newDiv)
-    document.body.removeChild(newButton)
-    i.value = null
-}
+        document.body.removeChild(newDiv)
+        document.body.removeChild(newButton)
+        i.value = null
+    }
 }
