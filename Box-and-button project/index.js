3689228b0eafb571b70323692dfb17811e91e6cb
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2023-01-18 19:53:25 -0500

    Notetaker

--- Box-and-button project/index.js
+++ Box-and-button project/index.js
@@ -1,8 +1,8 @@
 var button = document.querySelector('button');
 const celc = document.getElementById('tbuser');
 const out1 = document.getElementById('output1');
+const cbox = document.querySelector('checkbox')
 let faren = 0;
-
 function fub1() {
     faren = celc.value * (9/5) + 32;
     out1.innerHTML = faren + '°F';
---
+++ Note Taker/index.js
@@ -0,0 +1,16 @@
+let b = document.getElementById('comuner')
+let i = document.getElementById('noteInput')
+
+function commitTheNote() {
+    let newDiv = document.createElement("div")
+    let newButton = document.createElement("button")
+    newButton.placeholder = 'X'
+    newDiv.innerHTML = i.value;
+    document.body.appendChild(newDiv)
+    document.body.appendChild(newButton)
+    newButton.onclick = function deleteNewDiv() {
+    document.body.removeChild(newDiv)
+    document.body.removeChild(newButton)
+    i.value = null
+}
+}
