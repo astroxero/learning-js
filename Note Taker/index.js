6ca7789e5e47682498650b3c74b394910f0141cc
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2023-01-19 15:23:05 -0500

    styyle

--- Note Taker/index.js
+++ Note Taker/index.js
@@ -3,10 +3,11 @@ let i = document.getElementById('noteInput')
 
 function commitTheNote() {
     if (i.value != "") {
-        let newDiv = document.createElement("div")
+    let newDiv = document.createElement("div")
     let newButton = document.createElement("button")
     newButton.innerText = 'X'
-    newButton.class="padme"
+    newButton.className = "padme"
+    newDiv.className = "padme"
     newDiv.innerHTML = i.value;
     document.body.appendChild(newDiv)
     document.body.appendChild(newButton)
