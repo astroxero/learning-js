d4f374647e8e7b4fa48b0cee57978bb907699509
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2023-01-20 22:12:58 -0500

    box setup

--- F to C converter/index.js
+++ F to C converter/index.js
@@ -1,4 +1,5 @@
 var button = document.querySelector('button');
+let change1 = document.querySelector('div');
 const faren = document.getElementById('tbuser');
 const out1 = document.getElementById('output1');
 const cbox = document.querySelector('checkbox')
@@ -6,4 +7,5 @@ let celc = 0;
 function fub1() {
     celc = (faren.value - 32) * (5/9);
     out1.innerHTML = celc + '°C';
+    change1.style.background = 0,0,0;
 }
