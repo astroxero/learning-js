6b9abf5025d34d32a5a0514f38b1f1e82a87d1e7
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2023-01-21 12:57:39 -0500

    what to wear added and fixed

--- C & F converter/index.js
+++ C & F converter/index.js
@@ -14,18 +14,20 @@ let farenval = outie;
 function convert1() {
     if (ftoc == false) {
         outie = innie.value * (9/5) + 32 ;
+        farenval = outie;
         out1.innerHTML = outie + '°F';
-
         red = Math.floor(outie / 100 * 255);
         blue = Math.floor((100 - outie)/100 * 255);
         green = Math.floor(((50 - Math.abs (50 - outie)) / 50) * 196);
+        farenval = outie;
     } else {
         outie = (innie.value - 32) * (5/9);
         out1.innerHTML = outie + '°C';
 
         red = Math.floor(innie.value / 100 * 255);
         blue = Math.floor((100 - innie.value)/100 * 255);
-        green = Math.floor(((50 - Math.abs (50 - innie.value)) / 50) * 196);
+        green = Math.floor(((50 - Math.abs (50 - innie.value)) / 50) * 220);
+        farenval = innie.value;
     }
     
     change1.style.background = `rgb(${red}, ${green}, ${blue})`;
@@ -39,20 +41,32 @@ function convert1() {
     } else if (farenval >= 20 && farenval < 30) {
         out2.innerHTML = 'Wear a fleece, a jacket, and some gloves.'
     } else if (farenval >= 30 && farenval < 40) {
-        out2.innerHTML = 'Wear a jacket,a fleece, and some gloves.'
-    } else
+        out2.innerHTML = 'Break out the winter coat (and maybe a sweater)'
+    } else if (farenval >= 40 && farenval < 50) {
+        out2.innerHTML = 'Just a light jacket'
+    } else if (farenval >= 50 && farenval < 60) {
+        out2.innerHTML = 'Sweater Weather!'
+    } else if (farenval >= 60 && farenval < 70) {
+        out2.innerHTML = 'Comfort zone, no coat or sweater needed!'
+    } else if (farenval >= 70 && farenval < 80) {
+        out2.innerHTML = "Time for short sleeves, it's beautiful!"
+    } else if (farenval >= 80 && farenval < 90) {
+        out2.innerHTML = "It's getting hot! Go sleeveless!"
+    } else if (farenval >= 90 && farenval < 100) {
+        out2.innerHTML = "It's just too hot, stay hydrated and good luck!"
+    } else if (farenval >100) {
+        out2.innerHTML = "You're in hell! Head to the mountains!"
+    }
 }
 
 function switch1() {
     if (ftoc == false) {
         ftoc = true
-        farenval = innie.value;
         innie.placeholder = 'Enter Fahrenheit'
         innie.value = '';
         out1.innerHTML = null
     } else {
         ftoc = false
-        farenval = outie;
         innie.placeholder = 'Enter Celcius'
         innie.value = '';
         out1.innerHTML = null
