6ba70dfffdcbb7aaf00ee06178387c7a6ae5225b
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2023-01-31 19:05:35 -0500

    Finished!

---
+++ Quiz/CTab/CTab/index.js
@@ -0,0 +1,12 @@
+const corB = document.getElementById('corry')
+const inB = document.getElementById('inny')
+
+inB.onclick = function incorrectSwitch() {
+    console.log('coorectumundo!')
+    window.location.href = "../../ITab/index.html"
+}
+corB.onclick = function correctSwitch() {
+    console.log('coorectumundo!')
+    window.location.href = "./CTab/index.html"
+}
+
